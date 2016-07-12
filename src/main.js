554966198fa1d97e6696a791a48861cd35e04ae8
elasim <lequntus@hotmail.com>
2016-07-12 19:53:34 +0900

    add missing es6 polyfills

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
+import 'core-js/fn/array/find-index';
+import 'core-js/fn/object/assign';
 
 render(<App/>, document.getElementById('root'));
 
